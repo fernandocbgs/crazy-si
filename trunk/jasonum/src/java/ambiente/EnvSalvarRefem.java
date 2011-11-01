f371c2f6f0ea194e215e44d5031026bdf3da8172
eme.vbnet@gmail.com <eme.vbnet@gmail.com@228c13fa-f047-207c-ca2f-31547fb546eb>
2011-11-01 00:45:13 +0000

    Projeto Jason que consegue se comunicar com um robo no robocode !!!!! Funcionando: Sim ainda esta simples, ele controla um robo com a finalidade de chegar perto do robo refem que esta parado falta adicionar o robo que irá fazer o bloqueio

--- trunk/jasonum/src/java/ambiente/EnvSalvarRefem.java
+++ trunk/jasonum/src/java/ambiente/EnvSalvarRefem.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 import DadosRobos.DadosRobos;
+import Matematica.CalculoVetores;
 import tcp.TCPClient;
 import jason.asSyntax.Literal;
 import jason.asSyntax.Structure;
@@ -17,7 +18,7 @@ public class EnvSalvarRefem extends Environment {
 	private TCPClient[] _tcpcli;
 	//private int _portaServidor = 7896;
 	
-//	public static final String agSave = "agRobotSaver";
+	public static final String agSave = "agRobotSaver";
 //	public static final String agRefem = "agRefem";
 //	public static final String agInimigo = "agInimigo";
 	
@@ -110,8 +111,8 @@ public class EnvSalvarRefem extends Environment {
         
         if (r1 == null || r2 == null) {return;}
         
-        Literal pos1 = Literal.parseLiteral("pos("+r1.getNomeRobo()+"," + r1.getX() + "," + r1.getY() + ")");
-        Literal pos2 = Literal.parseLiteral("pos("+r2.getNomeRobo()+"," + r2.getX() + "," + r2.getX() + ")");
+        Literal pos1 = Literal.parseLiteral("pos("+agSave+"," + r1.getX() + "," + r1.getY() + ")");
+        Literal pos2 = Literal.parseLiteral("pos("+agSave+"," + r2.getX() + "," + r2.getX() + ")");
         
         addPercept(pos1);
         addPercept(pos2);
@@ -193,9 +194,11 @@ public class EnvSalvarRefem extends Environment {
             //Location agInimigoL = getAgPos(2); //inimigo
             
             if (pertoRefem()){
+            	//System.out.println("Esta PERTO !!!!!");
             	//adiciona a percepção que chegou perto do refem, então ele deve parar
             	//a caminhada em direção à ele
-            	addPercept(r1.getNomeRobo(), Literal.parseLiteral("pertoRefem."));
+            	//addPercept(r1.getNomeRobo(), Literal.parseLiteral("pertoRefem."));
+            	addPercept(agSave,Literal.parseLiteral("pertoRefem."));
             	return;
             }
 
@@ -238,137 +241,94 @@ public class EnvSalvarRefem extends Environment {
 //            List<String> ordens = new ArrayList<String>();
 //            ordens.add("5"); 
 //            ordens.add(agSaveL.x + "");
-            enviarOrdem(new CalculosRoboCode(agSaveL, agRefemL).getOrdems(), 0);
+            
+            //agSaveL, agRefemL
+            List<String> ordems = new CalculosRoboCode().getOrdems();
+            if (ordems != null)
+            	enviarOrdem(ordems, 0);
             
         }
         
         private boolean pertoRefem() {
-        	Location agSaveL = getAgPos(0); //agent Save
-            Location agRefemL = getAgPos(1); //refem
-        	return ((agSaveL.x == agRefemL.x+1 || agSaveL.x == agRefemL.x) 
-            		&& (agSaveL.y == agRefemL.y+1 || agSaveL.y == agRefemL.y));
-        }
-        
-        
-        
-//        /**
-//         * @maxDistance a distância máxima a ser levada em consideração
-//         * */
-//        private boolean pertoInimigo(int maxDistance) {
+        	atualizarDadosRobosViaTCP();
 //        	Location agSaveL = getAgPos(0); //agent Save
-//            Location agInimigoL = getAgPos(2); //inimigo
-//        	return ((agSaveL.x == agInimigoL.x || agSaveL.x == agInimigoL.x+maxDistance) 
-//            		&& (agSaveL.y == agInimigoL.y || agSaveL.y == agInimigoL.y+maxDistance));
-//        }
-        
-        
-        //------------------------ não usado
-        
- 
+//            Location agRefemL = getAgPos(1); //refem
+//        	return ((agSaveL.x == agRefemL.x+1 || agSaveL.x == agRefemL.x) 
+//            		&& (agSaveL.y == agRefemL.y+1 || agSaveL.y == agRefemL.y));
+        	double dis = Matematica.CalculoVetores.distanciaPontos(r1.getX(), r1.getY(),r2.getX(), r2.getY());
+        	return dis <= 80;
+        }
+
     }
     
     //-----------------------------------------------------------
     class CalculosRoboCode {
-    	private Location _rSalvador, _rRefem;
+    	
 
-    	public CalculosRoboCode(Location rSal, Location rRefem){
-    		_rSalvador = rSal; _rRefem = rRefem;
-    	}
+    	public CalculosRoboCode(){}
+    	
+//    	public CalculosRoboCode(Location rSal, Location rRefem){
+//    		_rSalvador = rSal; _rRefem = rRefem;
+//    	}
     	
     	public List<String> getOrdems(){
     		List<String> ordens = new ArrayList<String>();
     		
-    		if (estaAnguloCerto()) {
-    			//segue em frente
-    			ordens.add("5");
-    			ordens.add("50"); //50 unidades
-    		} else {
-    			ordens.add("4"); //virar direita
-    			
-    			double angulo = getAngulo();
-    			System.out.println("angulo: " + angulo);
-    			
-    			ordens.add(""+ getAnaliseValor(angulo, r1.getHeading())); 
-    		}
     		
-    		//Math.abs
+			ordens.add("3"); //virar esquerda
+			ordens.add(""+ getAngulo());
+			
+			double distancia = getDistanciaRefem();
+			System.out.println("distancia: " + distancia);
+			if (distancia > 75) {distancia-=75;} 
+			else {return ordens;} // não tem mais o que fazer
+			
+			ordens.add("5");
+			//ordens.add(distancia + "");
+			ordens.add(50 + ""); //50 unidades
+    		
+//    		System.out.println("ajustouAngulo: " + ajustouAngulo);
+//    		
+//    		if (/*estaAnguloCerto() &&*/ ajustouAngulo) {
+//    			//segue em frente
+//    			ordens.add("5");
+//    			ordens.add((getDistanciaRefem()-50) + ""); //50 unidades
+//    			
+//    			ajustouAngulo = false;
+//    		} else {
+//    			double angulo = getAngulo();
+//    			if (angulo < 0 && CalculoVetores.pertoParede(Width, Height, r1.getX(), r1.getY())) {
+//    				angulo = 0;
+//    			}
+//    			System.out.println("angulo: " + angulo);
+//    			
+//    			ordens.add("3"); //virar esquerda
+//    			ordens.add(""+ angulo);
+//    			
+//    			ajustouAngulo = true;
+//    		}
     		
     		return ordens;
     	}
     	
-    	private boolean estaAnguloCerto(){
-    		//verifica se esta no angulo certo
-    		return r1.getHeading() == getAngulo();
+    	public double getDistanciaRefem() {
+    		atualizarDadosRobosViaTCP();
+    		return Matematica.CalculoVetores.distanciaPontos(r1.getX(), r1.getY(),r2.getX(), r2.getY());
     	}
     	
-    	private double getAnaliseValor(double angulo, double heading){
-    		double vlr;
-    		vlr = angulo - heading;
-    		//informa o lado à ser rotacionado
-    		//if (getHeading() < headingEscolhido) {if (vlr<0) vlr *=-1;}
-    		return vlr;
-    	}
+//    	private boolean estaAnguloCerto(){
+//    		//verifica se esta no angulo certo
+//    		return r1.getHeading() == getAngulo() && ajustouAngulo;
+//    	}
     	
     	private double getAngulo(){
     		double rt= 0.0;
-    		rt = Matematica.CalculoVetores.AnguloTeste( 
-    				new Matematica.XY(r1.getX(), r1.getY()),
-    				new Matematica.XY(r2.getX(), r2.getY())
-    			  );
-    		System.out.println("Retorno: " + rt);
+    		rt = CalculoVetores.getAnguloDoisPontos(
+    				r1.getX(), r1.getY(),r2.getX(), r2.getY(),
+    				r1.getHeading()
+    		 	);
     		return rt;
     	}
-    	
-//    	private double getAngulo(){
-//    		return getAngulo(r1.getX(), r1.getY(), r2.getX(), r2.getY());
-//    	}
-//    	
-//    	private double getAngulo(double x1, double y1, double x2, double y2){
-//    		
-//    		System.out.println(x1+"-"+y1+"-"+x2+"-"+y2);
-//    		return Math.acos(x1*x2+y1*y2 / Math.sqrt(x1*x1 + y1*y1)*Math.sqrt(x2*y2 + y2*y2));
-//    	}
-        
-//    	/**Move in the direction of an x and y coordinate**/
-//    	private double goTo(double x, double y, double heading) {
-//    	    double dist = 20; 
-//    	    double angle = Math.toDegrees(absbearing(Width,Height,x,y));
-//    	    double r = turnTo(angle, heading);
-//    	    return dist * r;
-//    	}
-
-//    	/**Turns the shortest angle possible to come to a heading, then returns 
-//    	the direction the bot needs to move in.**/
-//    	private int turnTo(double x, double y, double heading) {
-//    		double angle = Math.toDegrees(absbearing(Width,Height,x,y)); //absbearing
-//    	    double ang;
-//    	    int dir;
-//    	    //ang = normalisebearing(heading - angle);
-//    	    ang = getAnaliseValor(angle, heading);
-//    	    if (ang > 90) {
-//    	        ang -= 180;
-//    	        dir = -1;
-//    	    }
-//    	    else if (ang < -90) {
-//    	        ang += 180;
-//    	        dir = -1;
-//    	    }
-//    	    else {
-//    	        dir = 1;
-//    	    }
-//    	    setTurnLeft(ang);
-//    	    return dir;
-//    	}
-    	
-//    	/**
-//    	 * Returns the distance between two points
-//    	 * */
-//    	private double getDistancia(double x1,double y1, double x2,double y2) {
-//    	    double x = x2-x1;
-//    	    double y = y2-y1;
-//    	    double range = Math.sqrt(x*x + y*y);
-//    	    return range;	
-//    	}
     	
     }
     
